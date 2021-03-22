987b237d401e9ca12cc0391aa0be7be98068a437
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-03-23 03:38:33 +0530

    Added tests folder

---
+++ tests/core/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <cmocka.h>
+
+
+int main()
+{
+    int result = 0;
+
+    return result;
+}
