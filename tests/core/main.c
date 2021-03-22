25c14c77889ab7153be28b3ec811ef1d6b763fd0
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-03-23 03:53:00 +0530

    Added template functions for unit tests

--- tests/core/main.c
+++ tests/core/main.c
@@ -1,3 +1,6 @@
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
+
 #include <stdlib.h>
 
 #include <setjmp.h>
@@ -7,10 +10,17 @@
 
 #include <cmocka.h>
 
+#include "vt_test_definitions.h"
 
 int main()
 {
     int result = 0;
+
+    // every test function returns the number of tests failed, 0 means success (there shouldn't be
+    // negative numbers
+    result += test_vt_fingerprint();
+    result += test_vt_database();
+    result += test_vt_sensor();
 
     return result;
 }
---
+++ tests/core/test_vt_database.c
@@ -0,0 +1,13 @@
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
+
+#include "vt_test_definitions.h"
+
+#include <setjmp.h>
+#include <stdarg.h>
+
+#include <cmocka.h>
+
+int test_vt_database()
+{
+}
