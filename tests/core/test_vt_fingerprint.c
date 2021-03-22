ad947ec0b4bc46e2c24f1f54b0e89625b82d3249
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-03-23 04:26:32 +0530

    Added unit test callback functions for vt_fingerprint

--- tests/core/test_vt_fingerprint.c
+++ tests/core/test_vt_fingerprint.c
@@ -8,7 +8,58 @@
 
 #include <cmocka.h>
 
+static void test_vt_fingerprint_calculate_maximum_index(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_fingerprint_calculate_minimum_index(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_fingerprint_calculate_calculate_37index(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_fingerprint_calculate_falltime_pearsoncoefficient(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_fingerprint_calculate_calculate_rankify(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_fingerprint_calculate_shape(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_fingerprint_evaluate_correlation_coefficient(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_fingerprint_evaluate_nrmse(void** state)
+{
+    (void)state;
+}
+
 int test_vt_fingerprint()
 {
-    return 0;
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_vt_fingerprint_calculate_maximum_index),
+        cmocka_unit_test(test_vt_fingerprint_calculate_minimum_index),
+        cmocka_unit_test(test_vt_fingerprint_calculate_calculate_37index),
+        cmocka_unit_test(test_vt_fingerprint_calculate_falltime_pearsoncoefficient),
+        cmocka_unit_test(test_vt_fingerprint_calculate_calculate_rankify),
+        cmocka_unit_test(test_vt_fingerprint_calculate_shape),
+        cmocka_unit_test(test_vt_fingerprint_evaluate_correlation_coefficient),
+        cmocka_unit_test(test_vt_fingerprint_evaluate_nrmse),
+    };
+
+    return cmocka_run_group_tests_name("vt_core_fingerprint", tests, NULL, NULL);
 }
