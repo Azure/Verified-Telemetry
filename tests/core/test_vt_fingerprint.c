ad67130d60f782e4a58ffe9e758bc819ec17b04d
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-03-26 22:20:39 +0530

    Removed redundant test cases

--- tests/core/test_vt_fingerprint.c
+++ tests/core/test_vt_fingerprint.c
@@ -15,8 +15,6 @@
 
 static int curve_zeroes[TEST_ARRAY_LENGTH]               = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 static int curve_constant[TEST_ARRAY_LENGTH]             = {5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5};
-static int curve_exponential_rise[TEST_ARRAY_LENGTH]     = {1};
-static int curve_exponential_fall[TEST_ARRAY_LENGTH]     = {1};
 static int curve_triagular[TEST_ARRAY_LENGTH]            = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
 
 static void test_vt_fingerprint_calculate_maximum_index(void** state)
@@ -25,8 +23,6 @@ static void test_vt_fingerprint_calculate_maximum_index(void** state)
 
     assert_int_equal(_vt_fingerprint_calculate_maximum_index(curve_zeroes, TEST_ARRAY_LENGTH), 0);
     assert_int_equal(_vt_fingerprint_calculate_maximum_index(curve_constant, TEST_ARRAY_LENGTH), 0);
-    //assert_int_equal(_vt_fingerprint_calculate_maximum_index(curve_exponential_rise, TEST_ARRAY_LENGTH), 100);
-    //assert_int_equal(_vt_fingerprint_calculate_maximum_index(curve_exponential_fall, TEST_ARRAY_LENGTH), 0);
     assert_int_equal(_vt_fingerprint_calculate_maximum_index(curve_triagular, TEST_ARRAY_LENGTH), 49);
 
 }
