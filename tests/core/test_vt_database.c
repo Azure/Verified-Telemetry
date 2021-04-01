67155b5d7874e8b901277821766659bcc4d3d742
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-04-01 18:04:40 +0530

    Updated unit test templates for database module

--- tests/core/test_vt_database.c
+++ tests/core/test_vt_database.c
@@ -12,6 +12,7 @@
 #include <cmocka.h>
 
 
+// Initialize
 
 static void test_vt_database_initialize_fingerprintdb(void** state)
 {
@@ -28,6 +29,17 @@ static void test_vt_database_initialize_pearsoncoefficientdb(void** state)
     (void)state;
 }
 
+
+// Store
+
+static void test_vt_database_store_fingerprint(void** state)
+{
+    (void)state;
+
+    //Static Function
+
+}
+
 static void test_vt_database_store_falltime(void** state)
 {
     (void)state;
@@ -39,10 +51,39 @@ static void test_vt_database_store_pearsoncoefficient(void** state)
 }
 
 
-static void test_vt_database_check_pearson_falltime_availability(void** state)
+static void test_vt_database_store_flash(void** state)
+{
+    (void)state;
+
+    //Static Function
+
+}
+
+// Clear
+
+static void test_vt_database_clear(void** state)
 {
     (void)state;
 }
+
+// Fetch
+
+static void test_vt_database_fingerprint_fetch(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_database_falltime_fetch(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_database_pearsoncoefficient_fetch(void** state)
+{
+    (void)state;
+}
+
+// Evaluate
 
 static void test_vt_database_evaluate_nrmse(void** state)
 {
@@ -54,6 +95,27 @@ static void test_vt_database_evaluate_pearson_falltime(void** state)
     (void)state;
 }
 
+static void test_vt_database_evaluate_check_pearson_falltime_availability(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_database_evaluate_falltime_nearestindex_search(void** state)
+{
+    (void)state;
+
+    //Static Function
+
+}
+
+static void test_vt_database_evaluate_pearsoncoefficient_falltimeindex_search(void** state)
+{
+    (void)state;
+
+    //Static Function
+
+}
+
 
 int test_vt_database()
 {
@@ -61,11 +123,19 @@ int test_vt_database()
         cmocka_unit_test(test_vt_database_initialize_fingerprintdb),
         cmocka_unit_test(test_vt_database_initialize_falltimedb),
         cmocka_unit_test(test_vt_database_initialize_pearsoncoefficientdb),
+        cmocka_unit_test(test_vt_database_store_fingerprint),
         cmocka_unit_test(test_vt_database_store_falltime),
         cmocka_unit_test(test_vt_database_store_pearsoncoefficient),
-        cmocka_unit_test(test_vt_database_check_pearson_falltime_availability),
+        cmocka_unit_test(test_vt_database_store_flash),
+        cmocka_unit_test(test_vt_database_clear),
+        cmocka_unit_test(test_vt_database_fingerprint_fetch),
+        cmocka_unit_test(test_vt_database_falltime_fetch),
+        cmocka_unit_test(test_vt_database_pearsoncoefficient_fetch),
         cmocka_unit_test(test_vt_database_evaluate_nrmse),
         cmocka_unit_test(test_vt_database_evaluate_pearson_falltime),
+        cmocka_unit_test(test_vt_database_evaluate_check_pearson_falltime_availability),
+        cmocka_unit_test(test_vt_database_evaluate_falltime_nearestindex_search),
+        cmocka_unit_test(test_vt_database_evaluate_pearsoncoefficient_falltimeindex_search),
     };
 
     return cmocka_run_group_tests_name("vt_core_database", tests, NULL, NULL);
