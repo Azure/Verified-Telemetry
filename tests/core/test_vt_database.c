8961a1af4138a5651f2cdf9e114cfd39df236fb1
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-04-01 19:01:38 +0530

    Added unit tests for database initialize functions

--- tests/core/test_vt_database.c
+++ tests/core/test_vt_database.c
@@ -11,24 +11,62 @@
 
 #include <cmocka.h>
 
-
 // Initialize
+
+VT_DATABASE test_database_ptr;
+
+static int vt_database_setup(void** state)
+{
+    test_database_ptr._vt_total_fingerprints          = 1;
+    test_database_ptr._vt_fingerprintdb[0][0]         = 1;
+    test_database_ptr._vt_total_falltime              = 1;
+    test_database_ptr._vt_falltimedb[0][0]            = 1;
+    test_database_ptr._vt_total_pearson_coefficient   = 1;
+    test_database_ptr._vt_pearson_coefficientdb[0][0] = 1;
+
+    return 0;
+
+}
 
 static void test_vt_database_initialize_fingerprintdb(void** state)
 {
     (void)state;
+
+    assert_int_equal(test_database_ptr._vt_total_fingerprints, 1);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][0], 1);
+
+    _vt_database_initialize_fingerprintdb(&test_database_ptr);
+
+    assert_int_equal(test_database_ptr._vt_total_fingerprints, 0);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][0], 0);
+
 }
 
 static void test_vt_database_initialize_falltimedb(void** state)
 {
     (void)state;
+
+    assert_int_equal(test_database_ptr._vt_total_falltime, 1);
+    assert_int_equal(test_database_ptr._vt_falltimedb[0][0], 1);
+
+    _vt_database_initialize_falltimedb(&test_database_ptr);
+
+    assert_int_equal(test_database_ptr._vt_total_falltime, 0);
+    assert_int_equal(test_database_ptr._vt_falltimedb[0][0], 0);
 }
 
 static void test_vt_database_initialize_pearsoncoefficientdb(void** state)
 {
     (void)state;
-}
 
+    assert_int_equal(test_database_ptr._vt_total_pearson_coefficient, 1);
+    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][0], 1, 0.001);
+
+    _vt_database_initialize_pearsoncoefficientdb(&test_database_ptr);
+
+    assert_int_equal(test_database_ptr._vt_total_pearson_coefficient, 0);
+    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][0], 0, 0.001);
+}
 
 // Store
 
@@ -36,8 +74,7 @@ static void test_vt_database_store_fingerprint(void** state)
 {
     (void)state;
 
-    //Static Function
-
+    // Static Function
 }
 
 static void test_vt_database_store_falltime(void** state)
@@ -50,13 +87,11 @@ static void test_vt_database_store_pearsoncoefficient(void** state)
     (void)state;
 }
 
-
 static void test_vt_database_store_flash(void** state)
 {
     (void)state;
 
-    //Static Function
-
+    // Static Function
 }
 
 // Clear
@@ -104,18 +139,15 @@ static void test_vt_database_evaluate_falltime_nearestindex_search(void** state)
 {
     (void)state;
 
-    //Static Function
-
+    // Static Function
 }
 
 static void test_vt_database_evaluate_pearsoncoefficient_falltimeindex_search(void** state)
 {
     (void)state;
 
-    //Static Function
-
+    // Static Function
 }
-
 
 int test_vt_database()
 {
@@ -138,5 +170,5 @@ int test_vt_database()
         cmocka_unit_test(test_vt_database_evaluate_pearsoncoefficient_falltimeindex_search),
     };
 
-    return cmocka_run_group_tests_name("vt_core_database", tests, NULL, NULL);
+    return cmocka_run_group_tests_name("vt_core_database", tests, vt_database_setup, NULL);
 }
