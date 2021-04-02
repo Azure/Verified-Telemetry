c69322d05cace25e89180a5d74a92e159b9b2280
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-04-02 16:22:54 +0530

    Added Unit tests for database evaluate module

--- tests/core/test_vt_database.c
+++ tests/core/test_vt_database.c
@@ -17,20 +17,34 @@ static uint32_t curve_triagular[TEST_ARRAY_LENGTH]            = {1,2,3,4,5,6,7,8
 static uint32_t curve_exponential_fall[TEST_ARRAY_LENGTH]     = {1000,989,980,970,960,950,941,931,922,913,903,894,885,876,868,859,850,842,833,825,817,808,800,792,784,776,769,761,753,746,738,731,723,716,709,702,695,688,681,674,667,660,654,647,641,634,628,622,615,609,603,597,591,585,579,573,567,562,556,551,545,540,534,529,523,518,513,508,503,498,493,488,483,478,473,468,464,459,454,450,445,441,436,432,428,423,419,415,411,406,402,398,394,390,386,383,379,375,371,367};
 
 
+
 VT_DATABASE test_database_ptr;
 
 // Test Fixtures
 static int vt_database_set(void** state)
 {
-    test_database_ptr._vt_total_fingerprints          = 1;
-    test_database_ptr._vt_fingerprintdb[0][0]         = 1;
-    test_database_ptr._vt_fingerprintdb[0][1]         = 1;
-    test_database_ptr._vt_total_falltime              = 1;
-    test_database_ptr._vt_falltimedb[0][0]            = 1;
-    test_database_ptr._vt_falltimedb[0][1]            = 1;
-    test_database_ptr._vt_total_pearson_coefficient   = 1;
-    test_database_ptr._vt_pearson_coefficientdb[0][0] = 1;
-    test_database_ptr._vt_pearson_coefficientdb[0][1] = 1;
+    test_database_ptr._vt_total_fingerprints  = 3;
+    test_database_ptr._vt_fingerprintdb[0][0] = 23;
+    test_database_ptr._vt_fingerprintdb[0][1] = 100;
+    memcpy(&(test_database_ptr._vt_fingerprintdb[0][2]), curve_exponential_fall, (sizeof(curve_exponential_fall)));
+    test_database_ptr._vt_fingerprintdb[1][0] = 12;
+    test_database_ptr._vt_fingerprintdb[1][1] = 50;
+    memcpy(&(test_database_ptr._vt_fingerprintdb[1][2]), curve_triagular, (sizeof(curve_exponential_fall)));
+    test_database_ptr._vt_fingerprintdb[2][0] = 23;
+    test_database_ptr._vt_fingerprintdb[2][1] = 95;
+    memcpy(&(test_database_ptr._vt_fingerprintdb[2][2]), curve_exponential_fall, (sizeof(curve_exponential_fall)));
+    test_database_ptr._vt_total_falltime              = 3;
+    test_database_ptr._vt_falltimedb[0][0]            = 12;
+    test_database_ptr._vt_falltimedb[0][1]            = 1650;
+    test_database_ptr._vt_falltimedb[1][0]            = 23;
+    test_database_ptr._vt_falltimedb[1][1]            = 4940;
+    test_database_ptr._vt_falltimedb[2][0]            = 23;
+    test_database_ptr._vt_falltimedb[2][1]            = 5200;
+    test_database_ptr._vt_total_pearson_coefficient   = 2;
+    test_database_ptr._vt_pearson_coefficientdb[0][0] = 23;
+    test_database_ptr._vt_pearson_coefficientdb[0][1] = 0.998063;
+    test_database_ptr._vt_pearson_coefficientdb[1][0] = 12;
+    test_database_ptr._vt_pearson_coefficientdb[1][1] = 0.991134;
 
     return 0;
 }
@@ -52,10 +66,6 @@ static void test_vt_database_initialize_fingerprintdb(void** state)
 {
     (void)state;
 
-    assert_int_equal(test_database_ptr._vt_total_fingerprints, 1);
-    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][0], 1);
-    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][1], 1);
-
     _vt_database_initialize_fingerprintdb(&test_database_ptr);
 
     assert_int_equal(test_database_ptr._vt_total_fingerprints, 0);
@@ -66,10 +76,6 @@ static void test_vt_database_initialize_fingerprintdb(void** state)
 static void test_vt_database_initialize_falltimedb(void** state)
 {
     (void)state;
-
-    assert_int_equal(test_database_ptr._vt_total_falltime, 1);
-    assert_int_equal(test_database_ptr._vt_falltimedb[0][0], 1);
-    assert_int_equal(test_database_ptr._vt_falltimedb[0][1], 1);
 
     _vt_database_initialize_falltimedb(&test_database_ptr);
 
@@ -82,10 +88,6 @@ static void test_vt_database_initialize_pearsoncoefficientdb(void** state)
 {
     (void)state;
 
-    assert_int_equal(test_database_ptr._vt_total_pearson_coefficient, 1);
-    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][0], 1, 0.001);
-    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][1], 1, 0.001);
-
     _vt_database_initialize_pearsoncoefficientdb(&test_database_ptr);
 
     assert_int_equal(test_database_ptr._vt_total_pearson_coefficient, 0);
@@ -96,16 +98,6 @@ static void test_vt_database_initialize_pearsoncoefficientdb(void** state)
 static void test_vt_database_initialize(void** state)
 {
     (void)state;
-
-    assert_int_equal(test_database_ptr._vt_total_fingerprints, 1);
-    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][0], 1);
-    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][1], 1);
-    assert_int_equal(test_database_ptr._vt_total_falltime, 1);
-    assert_int_equal(test_database_ptr._vt_falltimedb[0][0], 1);
-    assert_int_equal(test_database_ptr._vt_falltimedb[0][1], 1);
-    assert_int_equal(test_database_ptr._vt_total_pearson_coefficient, 1);
-    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][0], 1, 0.001);
-    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][1], 1, 0.001);
 
     vt_database_initialize(&test_database_ptr, 0x00, 1);
 
@@ -209,7 +201,7 @@ static void test_vt_database_store(void** state)
     assert_int_equal(test_database_ptr._vt_fingerprintdb[0][1], 100);
     for (uint32_t i = 0; i < TEST_ARRAY_LENGTH; i++)
     {
-        assert_int_equal(test_database_ptr._vt_fingerprintdb[0][i+2], curve_exponential_fall[i]);
+        assert_int_equal(test_database_ptr._vt_fingerprintdb[0][i + 2], curve_exponential_fall[i]);
     }
     assert_int_equal(test_database_ptr._vt_total_falltime, 1);
     assert_int_equal(test_database_ptr._vt_falltimedb[0][0], 23);
@@ -217,6 +209,112 @@ static void test_vt_database_store(void** state)
     assert_int_equal(test_database_ptr._vt_total_pearson_coefficient, 1);
     assert_int_equal(test_database_ptr._vt_pearson_coefficientdb[0][0], 23);
     assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][1], 0.998063, 0.001);
+
+    assert_int_equal(vt_database_store(&test_database_ptr, curve_triagular, 50, 12), VT_SUCCESS);
+
+    assert_int_equal(test_database_ptr._vt_total_fingerprints, 2);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][0], 23);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][1], 100);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[1][0], 12);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[1][1], 50);
+    for (uint32_t i = 0; i < TEST_ARRAY_LENGTH; i++)
+    {
+        assert_int_equal(test_database_ptr._vt_fingerprintdb[0][i + 2], curve_exponential_fall[i]);
+        assert_int_equal(test_database_ptr._vt_fingerprintdb[1][i + 2], curve_triagular[i]);
+    }
+    assert_int_equal(test_database_ptr._vt_total_falltime, 2);
+    assert_int_equal(test_database_ptr._vt_falltimedb[0][0], 12);
+    assert_int_equal(test_database_ptr._vt_falltimedb[0][1], 1650);
+    assert_int_equal(test_database_ptr._vt_falltimedb[1][0], 23);
+    assert_int_equal(test_database_ptr._vt_falltimedb[1][1], 5200);
+    assert_int_equal(test_database_ptr._vt_total_pearson_coefficient, 2);
+    assert_int_equal(test_database_ptr._vt_pearson_coefficientdb[0][0], 23);
+    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][1], 0.998063, 0.001);
+    assert_int_equal(test_database_ptr._vt_pearson_coefficientdb[1][0], 12);
+    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[1][1], 0.991134, 0.001);
+
+    assert_int_equal(vt_database_store(&test_database_ptr, curve_exponential_fall, 95, 23), VT_SUCCESS);
+
+    assert_int_equal(test_database_ptr._vt_total_fingerprints, 3);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][0], 23);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[0][1], 100);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[1][0], 12);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[1][1], 50);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[2][0], 23);
+    assert_int_equal(test_database_ptr._vt_fingerprintdb[2][1], 95);
+    for (uint32_t i = 0; i < TEST_ARRAY_LENGTH; i++)
+    {
+        assert_int_equal(test_database_ptr._vt_fingerprintdb[0][i + 2], curve_exponential_fall[i]);
+        assert_int_equal(test_database_ptr._vt_fingerprintdb[1][i + 2], curve_triagular[i]);
+        assert_int_equal(test_database_ptr._vt_fingerprintdb[2][i + 2], curve_exponential_fall[i]);
+    }
+    assert_int_equal(test_database_ptr._vt_total_falltime, 3);
+    assert_int_equal(test_database_ptr._vt_falltimedb[0][0], 12);
+    assert_int_equal(test_database_ptr._vt_falltimedb[0][1], 1650);
+    assert_int_equal(test_database_ptr._vt_falltimedb[1][0], 23);
+    assert_int_equal(test_database_ptr._vt_falltimedb[1][1], 4940);
+    assert_int_equal(test_database_ptr._vt_falltimedb[2][0], 23);
+    assert_int_equal(test_database_ptr._vt_falltimedb[2][1], 5200);
+    assert_int_equal(test_database_ptr._vt_total_pearson_coefficient, 2);
+    assert_int_equal(test_database_ptr._vt_pearson_coefficientdb[0][0], 23);
+    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][1], 0.998063, 0.001);
+    assert_int_equal(test_database_ptr._vt_pearson_coefficientdb[1][0], 12);
+    assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[1][1], 0.991134, 0.001);
+}
+
+// Evaluate
+
+static void test_vt_database_evaluate_nrmse(void** state)
+{
+    (void)state;
+
+   assert_int_equal(_vt_database_evaluate_nrmse(&test_database_ptr,curve_exponential_fall),23);
+   assert_int_equal(_vt_database_evaluate_nrmse(&test_database_ptr,curve_triagular),12);
+}
+
+static void test_vt_database_evaluate_pearson_falltime(void** state)
+{
+    (void)state;
+
+    assert_int_equal(_vt_database_evaluate_pearson_falltime(&test_database_ptr, 5200, 0.998063), 23);
+    assert_int_equal(_vt_database_evaluate_pearson_falltime(&test_database_ptr, 4940, 0.998063), 23);
+    assert_int_equal(_vt_database_evaluate_pearson_falltime(&test_database_ptr, 1650, 0.991134), 12);
+}
+
+static void test_vt_database_evaluate_falltime_nearestindex_search(void** state)
+{
+    (void)state;
+
+    // Static Function
+}
+
+static void test_vt_database_evaluate_pearsoncoefficient_falltimeindex_search(void** state)
+{
+    (void)state;
+
+    // Static Function
+}
+
+// Fetch
+
+static void test_vt_database_fingerprint_fetch(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_database_falltime_fetch(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_database_pearsoncoefficient_fetch(void** state)
+{
+    (void)state;
+}
+
+static void test_vt_database_check_pearson_falltime_availability(void** state)
+{
+    (void)state;
 }
 
 // Clear
@@ -238,75 +336,27 @@ static void test_vt_database_clear(void** state)
     assert_float_equal(test_database_ptr._vt_pearson_coefficientdb[0][1], 0, 0.001);
 }
 
-// Fetch
-
-static void test_vt_database_fingerprint_fetch(void** state)
-{
-    (void)state;
-}
-
-static void test_vt_database_falltime_fetch(void** state)
-{
-    (void)state;
-}
-
-static void test_vt_database_pearsoncoefficient_fetch(void** state)
-{
-    (void)state;
-}
-
-// Evaluate
-
-static void test_vt_database_evaluate_nrmse(void** state)
-{
-    (void)state;
-}
-
-static void test_vt_database_evaluate_pearson_falltime(void** state)
-{
-    (void)state;
-}
-
-static void test_vt_database_evaluate_check_pearson_falltime_availability(void** state)
-{
-    (void)state;
-}
-
-static void test_vt_database_evaluate_falltime_nearestindex_search(void** state)
-{
-    (void)state;
-
-    // Static Function
-}
-
-static void test_vt_database_evaluate_pearsoncoefficient_falltimeindex_search(void** state)
-{
-    (void)state;
-
-    // Static Function
-}
-
 int test_vt_database()
 {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test_setup_teardown(test_vt_database_initialize_fingerprintdb, vt_database_set, vt_database_reset),
-        cmocka_unit_test_setup_teardown(test_vt_database_initialize_falltimedb, vt_database_set,vt_database_reset),
-        cmocka_unit_test_setup_teardown(test_vt_database_initialize_pearsoncoefficientdb, vt_database_set,vt_database_reset),
+        cmocka_unit_test_setup_teardown(test_vt_database_initialize_falltimedb, vt_database_set, vt_database_reset),
+        cmocka_unit_test_setup_teardown(test_vt_database_initialize_pearsoncoefficientdb, vt_database_set, vt_database_reset),
         cmocka_unit_test_setup_teardown(test_vt_database_initialize, vt_database_set, vt_database_reset),
         cmocka_unit_test(test_vt_database_store_fingerprint),
         cmocka_unit_test_setup_teardown(test_vt_database_store_falltime, vt_database_reset, vt_database_reset),
         cmocka_unit_test_setup_teardown(test_vt_database_store_pearsoncoefficient, vt_database_reset, vt_database_reset),
         cmocka_unit_test(test_vt_database_store_flash),
-        cmocka_unit_test_setup(test_vt_database_store, vt_database_reset),
-        cmocka_unit_test(test_vt_database_clear),
+        cmocka_unit_test_setup_teardown(test_vt_database_store, vt_database_reset, vt_database_reset),
+        cmocka_unit_test_setup_teardown(test_vt_database_evaluate_nrmse, vt_database_set, vt_database_reset),
+        cmocka_unit_test_setup_teardown(test_vt_database_evaluate_pearson_falltime, vt_database_set, vt_database_reset),
+        cmocka_unit_test_setup_teardown(test_vt_database_evaluate_falltime_nearestindex_search, vt_database_set, vt_database_reset),
+        cmocka_unit_test_setup_teardown(test_vt_database_evaluate_pearsoncoefficient_falltimeindex_search, vt_database_set, vt_database_reset),
         cmocka_unit_test(test_vt_database_fingerprint_fetch),
         cmocka_unit_test(test_vt_database_falltime_fetch),
         cmocka_unit_test(test_vt_database_pearsoncoefficient_fetch),
-        cmocka_unit_test(test_vt_database_evaluate_nrmse),
-        cmocka_unit_test(test_vt_database_evaluate_pearson_falltime),
-        cmocka_unit_test(test_vt_database_evaluate_check_pearson_falltime_availability),
-        cmocka_unit_test(test_vt_database_evaluate_falltime_nearestindex_search),
-        cmocka_unit_test(test_vt_database_evaluate_pearsoncoefficient_falltimeindex_search),
+        cmocka_unit_test(test_vt_database_check_pearson_falltime_availability),
+        cmocka_unit_test_setup(test_vt_database_clear, vt_database_set),
     };
 
     return cmocka_run_group_tests_name("vt_core_database", tests, NULL, NULL);
