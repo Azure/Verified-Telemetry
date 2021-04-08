7d0bb20e74331acbd08b366a246cb3345b96fee5
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-04-08 18:54:53 +0530

    Update test_vt_sensor.c

--- tests/core/test_vt_sensor.c
+++ tests/core/test_vt_sensor.c
@@ -4,6 +4,8 @@
 #include "vt_test_definitions.h"
 
 #include "vt_api.h"
+#include "vt_dsc.h"
+#include "vt_port.h"
 #include "vt_sensor.h"
 
 #include <setjmp.h>
@@ -15,7 +17,6 @@
 static void test_vt_sensor_initialize(void** state)
 {
     (void)state;
-
 }
 
 static void test_vt_sensor_read_value(void** state)
@@ -25,35 +26,36 @@ static void test_vt_sensor_read_value(void** state)
     VT_SENSOR x;
     uint32_t y;
 
-    vt_sensor_read_value(&x,&y);
+    will_return(__wrap__vt_dsc_adc_read, 23);
+    will_return(__wrap__vt_dsc_adc_read, 12);
+    
+    vt_sensor_read_value(&x, &y);
+    assert_int_equal(y,23);
+
+    vt_sensor_read_value(&x, &y);
+    assert_int_equal(y,7);
 
 }
 
 static void test_vt_sensor_read_fingerprint(void** state)
 {
     (void)state;
-
 }
 
 static void test_vt_sensor_read_status(void** state)
 {
     (void)state;
-
 }
 
 static void test_vt_sensor_calibrate(void** state)
 {
     (void)state;
-
 }
 
-int __wrap__vt_dsc_adc_read(int* adc_controller, int adc_channel, int* value)
+int __wrap__vt_dsc_adc_read(ADC_CONTROLLER_TYPEDEF* adc_controller, ADC_CHANNEL_TYPEDEF adc_channel, uint32_t* value)
 {
-    printf("\nIn Wrap");
-    printf("\nIn Wrap");
-    printf("\nIn Wrap");
-    printf("\nIn Wrap");
-    return 0;
+    *value = (uint32_t)mock();
+    return VT_PLATFORM_SUCCESS;
 }
 
 int test_vt_sensor()
