472e6fe80f3c941a91b69700448bb4af76a3f298
Ryan Winter <ryanwinter@outlook.com>
2021-05-17 17:15:31 -0700

    fixed doxygen for extern wrapper

--- inc/middleware/nx_vt_fallcurve_component.h
+++ inc/middleware/nx_vt_fallcurve_component.h
@@ -5,8 +5,7 @@
 #define NX_VT_FALLCURVE_COMPONENT_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #include "nx_azure_iot_json_reader.h"
@@ -15,66 +14,66 @@ extern "C"
 
 #include "vt_fc_api.h"
 
-    typedef struct NX_VT_FALLCURVE_COMPONENT_TAG
-    {
-        /* Name of this component */
-        UCHAR component_name_ptr[30];
+typedef struct NX_VT_FALLCURVE_COMPONENT_TAG
+{
+    /* Name of this component */
+    UCHAR component_name_ptr[30];
 
-        /* Component Name Length */
-        UINT component_name_length;
+    /* Component Name Length */
+    UINT component_name_length;
 
-        /* Telemetry associated with the fallcurve signature component*/
-        UCHAR associated_telemetry[100];
+    /* Telemetry associated with the fallcurve signature component*/
+    UCHAR associated_telemetry[100];
 
-        /* Status of the telemetry associated with the fallcurve signature component*/
-        bool telemetry_status;
+    /* Status of the telemetry associated with the fallcurve signature component*/
+    bool telemetry_status;
 
-        /* Fallcurve Object */
-        VT_FALLCURVE_OBJECT fc_object;
+    /* Fallcurve Object */
+    VT_FALLCURVE_OBJECT fc_object;
 
-        /* Status of reported properties sent  */
-        UINT property_sent;
+    /* Status of reported properties sent  */
+    UINT property_sent;
 
-        /* Stores on the scale of 0-100 how much Fingerprint Template can be trusted */
-        UINT template_confidence_metric;
+    /* Stores on the scale of 0-100 how much Fingerprint Template can be trusted */
+    UINT template_confidence_metric;
 
-        /* Compute sensor status when a global command is issued */
-        bool telemetry_status_auto_update;
+    /* Compute sensor status when a global command is issued */
+    bool telemetry_status_auto_update;
 
-    } NX_VT_FALLCURVE_COMPONENT;
+} NX_VT_FALLCURVE_COMPONENT;
 
-    UINT nx_vt_fallcurve_init(NX_VT_FALLCURVE_COMPONENT* handle,
-        UCHAR* component_name_ptr,
-        VT_DEVICE_DRIVER* device_driver,
-        VT_SENSOR_HANDLE* sensor_handle,
-        UCHAR* associated_telemetry,
-        bool telemetry_status_auto_update);
+UINT nx_vt_fallcurve_init(NX_VT_FALLCURVE_COMPONENT* handle,
+    UCHAR* component_name_ptr,
+    VT_DEVICE_DRIVER* device_driver,
+    VT_SENSOR_HANDLE* sensor_handle,
+    UCHAR* associated_telemetry,
+    bool telemetry_status_auto_update);
 
-    UINT nx_vt_fallcurve_telemetry_status_property(
-        NX_VT_FALLCURVE_COMPONENT* handle, NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr, bool* device_status);
+UINT nx_vt_fallcurve_telemetry_status_property(
+    NX_VT_FALLCURVE_COMPONENT* handle, NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr, bool* device_status);
 
-    UINT nx_vt_fallcurve_fingerprint_type_property(NX_VT_FALLCURVE_COMPONENT* handle, NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr);
+UINT nx_vt_fallcurve_fingerprint_type_property(NX_VT_FALLCURVE_COMPONENT* handle, NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr);
 
-    UINT nx_vt_fallcurve_process_command(NX_VT_FALLCURVE_COMPONENT* handle,
-        NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr,
-        UCHAR* component_name_ptr,
-        UINT component_name_length,
-        UCHAR* pnp_command_name_ptr,
-        UINT pnp_command_name_length,
-        NX_AZURE_IOT_JSON_READER* json_reader_ptr,
-        NX_AZURE_IOT_JSON_WRITER* json_response_ptr,
-        UINT* status_code);
+UINT nx_vt_fallcurve_process_command(NX_VT_FALLCURVE_COMPONENT* handle,
+    NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr,
+    UCHAR* component_name_ptr,
+    UINT component_name_length,
+    UCHAR* pnp_command_name_ptr,
+    UINT pnp_command_name_length,
+    NX_AZURE_IOT_JSON_READER* json_reader_ptr,
+    NX_AZURE_IOT_JSON_WRITER* json_response_ptr,
+    UINT* status_code);
 
-    UINT nx_vt_fallcurve_process_reported_property_sync(NX_VT_FALLCURVE_COMPONENT* handle,
-        NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr,
-        const UCHAR* component_name_ptr,
-        UINT component_name_length,
-        NX_AZURE_IOT_JSON_READER* name_value_reader_ptr,
-        UINT version);
+UINT nx_vt_fallcurve_process_reported_property_sync(NX_VT_FALLCURVE_COMPONENT* handle,
+    NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr,
+    const UCHAR* component_name_ptr,
+    UINT component_name_length,
+    NX_AZURE_IOT_JSON_READER* name_value_reader_ptr,
+    UINT version);
 
-    UINT nx_vt_fallcurve_compute_sensor_status_global(NX_VT_FALLCURVE_COMPONENT* handle, bool toggle_verified_telemetry);
+UINT nx_vt_fallcurve_compute_sensor_status_global(NX_VT_FALLCURVE_COMPONENT* handle, bool toggle_verified_telemetry);
 
-    // UINT nx_vt_fallcurve_compute_sensor_status(NX_VT_FALLCURVE_COMPONENT* handle);
+// UINT nx_vt_fallcurve_compute_sensor_status(NX_VT_FALLCURVE_COMPONENT* handle);
 
 #ifdef __cplusplus
 }
