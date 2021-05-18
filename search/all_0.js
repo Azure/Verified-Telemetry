9b55ee78ccba4313d89c94880ca58a2663a8f938
ryanwinter <ryanwinter@users.noreply.github.com>
2021-05-18 19:28:42 +0000

    deploy: 31f54de85cc5b0b59897ca50cc54fdac95ce5e55

--- search/all_0.js
+++ search/all_0.js
@@ -5,7 +5,13 @@ var searchData=
   ['nx_5fverified_5ftelemetry_5fdb_5ftag_2',['NX_VERIFIED_TELEMETRY_DB_TAG',['../struct_n_x___v_e_r_i_f_i_e_d___t_e_l_e_m_e_t_r_y___d_b___t_a_g.html',1,'']]],
   ['nx_5fvt_5ffallcurve_5fcomponent_2eh_3',['nx_vt_fallcurve_component.h',['../nx__vt__fallcurve__component_8h.html',1,'']]],
   ['nx_5fvt_5ffallcurve_5fcomponent_5ftag_4',['NX_VT_FALLCURVE_COMPONENT_TAG',['../struct_n_x___v_t___f_a_l_l_c_u_r_v_e___c_o_m_p_o_n_e_n_t___t_a_g.html',1,'']]],
-  ['nx_5fvt_5fmiddleware_5fhelper_2eh_5',['nx_vt_middleware_helper.h',['../nx__vt__middleware__helper_8h.html',1,'']]],
-  ['nx_5fvt_5fobject_5ftag_6',['NX_VT_OBJECT_TAG',['../struct_n_x___v_t___o_b_j_e_c_t___t_a_g.html',1,'']]],
-  ['nx_5fvt_5fsignature_5fcomponent_5funion_5ftag_7',['NX_VT_SIGNATURE_COMPONENT_UNION_TAG',['../union_n_x___v_t___s_i_g_n_a_t_u_r_e___c_o_m_p_o_n_e_n_t___u_n_i_o_n___t_a_g.html',1,'']]]
+  ['nx_5fvt_5ffallcurve_5finit_5',['nx_vt_fallcurve_init',['../nx__vt__fallcurve__component_8h.html#a26b42b9452a1855a306c94bd5d867891',1,'nx_vt_fallcurve_component.h']]],
+  ['nx_5fvt_5finit_6',['nx_vt_init',['../nx__verified__telemetry_8h.html#ad062c9c653e648444b86331c85461346',1,'nx_verified_telemetry.h']]],
+  ['nx_5fvt_5fmiddleware_5fhelper_2eh_7',['nx_vt_middleware_helper.h',['../nx__vt__middleware__helper_8h.html',1,'']]],
+  ['nx_5fvt_5fobject_5ftag_8',['NX_VT_OBJECT_TAG',['../struct_n_x___v_t___o_b_j_e_c_t___t_a_g.html',1,'']]],
+  ['nx_5fvt_5fprocess_5fcommand_9',['nx_vt_process_command',['../nx__verified__telemetry_8h.html#ae3eade7dce821075fbb798accfba9861',1,'nx_verified_telemetry.h']]],
+  ['nx_5fvt_5fprocess_5fproperty_5fupdate_10',['nx_vt_process_property_update',['../nx__verified__telemetry_8h.html#afe321e112e5f1352cd7565abfd2b29b0',1,'nx_verified_telemetry.h']]],
+  ['nx_5fvt_5fprocess_5freported_5fproperty_5fsync_11',['nx_vt_process_reported_property_sync',['../nx__verified__telemetry_8h.html#a1564061e99e94806333c9377258d0ca4',1,'nx_verified_telemetry.h']]],
+  ['nx_5fvt_5fproperties_12',['nx_vt_properties',['../nx__verified__telemetry_8h.html#acd4f204d322dd91d1343740775eba1f3',1,'nx_verified_telemetry.h']]],
+  ['nx_5fvt_5fsignature_5fcomponent_5funion_5ftag_13',['NX_VT_SIGNATURE_COMPONENT_UNION_TAG',['../union_n_x___v_t___s_i_g_n_a_t_u_r_e___c_o_m_p_o_n_e_n_t___u_n_i_o_n___t_a_g.html',1,'']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['vt_5fdevice_5fdriver_5fstruct_21',['VT_DEVICE_DRIVER_STRUCT',['../struct_v_t___d_e_v_i_c_e___d_r_i_v_e_r___s_t_r_u_c_t.html',1,'']]],
-  ['vt_5ffallcurve_5fdatabase_5fflattened_5fstruct_22',['VT_FALLCURVE_DATABASE_FLATTENED_STRUCT',['../struct_v_t___f_a_l_l_c_u_r_v_e___d_a_t_a_b_a_s_e___f_l_a_t_t_e_n_e_d___s_t_r_u_c_t.html',1,'']]],
-  ['vt_5ffallcurve_5fobject_5fstruct_23',['VT_FALLCURVE_OBJECT_STRUCT',['../struct_v_t___f_a_l_l_c_u_r_v_e___o_b_j_e_c_t___s_t_r_u_c_t.html',1,'']]],
-  ['vt_5fsensor_5fhandle_5fstruct_24',['VT_SENSOR_HANDLE_STRUCT',['../struct_v_t___s_e_n_s_o_r___h_a_n_d_l_e___s_t_r_u_c_t.html',1,'']]]
+  ['vt_5fdevice_5fdriver_5fstruct_27',['VT_DEVICE_DRIVER_STRUCT',['../struct_v_t___d_e_v_i_c_e___d_r_i_v_e_r___s_t_r_u_c_t.html',1,'']]],
+  ['vt_5ffallcurve_5fdatabase_5fflattened_5fstruct_28',['VT_FALLCURVE_DATABASE_FLATTENED_STRUCT',['../struct_v_t___f_a_l_l_c_u_r_v_e___d_a_t_a_b_a_s_e___f_l_a_t_t_e_n_e_d___s_t_r_u_c_t.html',1,'']]],
+  ['vt_5ffallcurve_5fobject_5fstruct_29',['VT_FALLCURVE_OBJECT_STRUCT',['../struct_v_t___f_a_l_l_c_u_r_v_e___o_b_j_e_c_t___s_t_r_u_c_t.html',1,'']]],
+  ['vt_5fsensor_5fhandle_5fstruct_30',['VT_SENSOR_HANDLE_STRUCT',['../struct_v_t___s_e_n_s_o_r___h_a_n_d_l_e___s_t_r_u_c_t.html',1,'']]]
 ];
