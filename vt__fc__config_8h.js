710202ffa861847c463428fe806d8109f91c506c
sumukhmarathe <sumukhmarathe@users.noreply.github.com>
2021-06-08 16:33:13 +0000

    deploy: 0e1d294c2fdb9f76d689f7087580d3f57095b0b4

--- vt__fc__config_8h.js
+++ vt__fc__config_8h.js
@@ -3,6 +3,7 @@ var vt__fc__config_8h =
     [ "VT_FC_MAX_FALLTIME_DEVIATION", "vt__fc__config_8h.html#a73ed72697249930e78540b6292beab96", null ],
     [ "VT_FC_MAX_SAMPLING_INTERVAL_US", "vt__fc__config_8h.html#a57dc86a435aa59793d1057800a66fb49", null ],
     [ "VT_FC_MAX_SHAPE_DEVIATION", "vt__fc__config_8h.html#a69d2d32ce5fe2fdcf9545e62eddd8189", null ],
+    [ "VT_FC_MAX_SHAPE_DEVIATION_FACTOR", "vt__fc__config_8h.html#a34388ae040fd7a7fb389dcc99345a7b7", null ],
     [ "VT_FC_MAX_SIGNATURES", "vt__fc__config_8h.html#a495e97f81ac4cff76c420942ecfe95b7", null ],
     [ "VT_FC_MIN_FALLTIME_DATAPOINTS", "vt__fc__config_8h.html#a7524111c77e2cce0750a253e669cabba", null ],
     [ "VT_FC_MIN_SAMPLING_INTERVAL_US", "vt__fc__config_8h.html#aa727dfb5621a9b34cd9315013f030f7f", null ],
