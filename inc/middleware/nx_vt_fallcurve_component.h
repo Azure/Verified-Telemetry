6c3fe8bf553d6a307edc43d7ca84864f00b05662
Shubhankar Handa <50367120+Cout-Of-The-Box@users.noreply.github.com>
2021-08-14 05:36:17 +0530

    Doxygen update (#48)

--- inc/middleware/nx_vt_fallcurve_component.h
+++ inc/middleware/nx_vt_fallcurve_component.h
@@ -45,14 +45,14 @@ typedef struct NX_VT_FALLCURVE_COMPONENT_TAG
 } NX_VT_FALLCURVE_COMPONENT;
 
 /**
- * @brief Initializes an instance of Verified Telemetry Information Interface.
+ * @brief Initializes an instance of Verified Telemetry Information Interface for fallcurve component.
  *
  * @param[in] handle The handle created by a call to the initialization function.
  * @param[in] component_name_ptr Name of the PNP component.
  * @param[in] device_driver The platform driver functions for interacting with the device hardware.
  * @param[in] sensor_handle The platform driver references for interacting with the device hardware.
  * @param[in] associated_telemetry Name of the telemetry associated with this component.
- * @param[in] telemetry_status_auto_update
+ * @param[in] telemetry_status_auto_update Bool to compute sensor status when a global command is issued.
  *
  * @retval NX_AZURE_IOT_SUCCESS upon success or an error code upon failure.
  */
@@ -63,10 +63,45 @@ UINT nx_vt_fallcurve_init(NX_VT_FALLCURVE_COMPONENT* handle,
     UCHAR* associated_telemetry,
     bool telemetry_status_auto_update);
 
+/**
+ * @brief Send "telemetryStatus" read-only property to IoT Hub.
+ *
+ * @param[in] handle The currentsense handle created by a call to the initialization function.
+ * @param[in] iotpnp_client_ptr Pointer to initialized Azure IoT PnP instance.
+ * @param[out] device_status Pointer to update the global bool showing if all devices are working or not.
+ *
+ * @retval NX_AZURE_IOT_SUCCESS upon success or an error code upon failure.
+ */
+
 UINT nx_vt_fallcurve_telemetry_status_property(
     NX_VT_FALLCURVE_COMPONENT* handle, NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr, bool* device_status);
 
+/**
+ * @brief Send "fingerprintType" read-only property to IoT Hub.
+ *
+ * @param[in] handle The currentsense handle created by a call to the initialization function.
+ * @param[in] iotpnp_client_ptr Pointer to initialized Azure IoT PnP instance.
+ *
+ * @retval NX_AZURE_IOT_SUCCESS upon success or an error code upon failure.
+ */
+
 UINT nx_vt_fallcurve_fingerprint_type_property(NX_VT_FALLCURVE_COMPONENT* handle, NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr);
+
+/**
+ * @brief Process commands related to fallcurve component eg. setResetFingerprintTemplate , retrainFingerprintTemplate.
+ *
+ * @param[in] handle The currentsense handle created by a call to the initialization function.
+ * @param[in] iotpnp_client_ptr Pointer to initialized Azure IoT PnP instance.
+ * @param[in] component_name_ptr Name of the component.
+ * @param[in] component_name_length Length of name of the component.
+ * @param[in] pnp_command_name_ptr Name of the command invoked.
+ * @param[in] pnp_command_name_length Length of name of the command invoked.
+ * @param[in] json_reader_ptr Pointer to read the JSON payload of command.
+ * @param[in] json_response_ptr Pointer to write response JSON payload.
+ * @param[out] status_code Status updated by function based on command execution.
+ *
+ * @retval NX_AZURE_IOT_SUCCESS upon success or an error code upon failure.
+ */
 
 UINT nx_vt_fallcurve_process_command(NX_VT_FALLCURVE_COMPONENT* handle,
     NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr,
@@ -78,12 +113,34 @@ UINT nx_vt_fallcurve_process_command(NX_VT_FALLCURVE_COMPONENT* handle,
     NX_AZURE_IOT_JSON_WRITER* json_response_ptr,
     UINT* status_code);
 
+/**
+ * @brief Synchronizes on-device fallcurve template database from the digital twin upon startup.
+ *
+ * @param[in] handle The currentsense handle created by a call to the initialization function.
+ * @param[in] iotpnp_client_ptr Pointer to initialized Azure IoT PnP instance.
+ * @param[in] component_name_ptr Name of the component.
+ * @param[in] component_name_length Length of name of the component.
+ * @param[in] name_value_reader_ptr Pointer to read the JSON payload of command.
+ * @param[in] version Property version stored in digital twin.
+ *
+ * @retval NX_AZURE_IOT_SUCCESS upon success or an error code upon failure.
+ */
+
 UINT nx_vt_fallcurve_process_reported_property_sync(NX_VT_FALLCURVE_COMPONENT* handle,
     NX_AZURE_IOT_PNP_CLIENT* iotpnp_client_ptr,
     const UCHAR* component_name_ptr,
     UINT component_name_length,
     NX_AZURE_IOT_JSON_READER* name_value_reader_ptr,
     UINT version);
+
+/**
+ * @brief Get status of the sensor related to this fallcurve component.
+ *
+ * @param[in] handle The currentsense handle created by a call to the initialization function.
+ * @param[in] toggle_verified_telemetry Bool value to enable VT for this component or not.
+ *
+ * @retval NX_AZURE_IOT_SUCCESS upon success or an error code upon failure.
+ */
 
 UINT nx_vt_fallcurve_compute_sensor_status_global(NX_VT_FALLCURVE_COMPONENT* handle, bool toggle_verified_telemetry);
 
--- inc/middleware/nx_vt_middleware_helper.h
+++ inc/middleware/nx_vt_middleware_helper.h
@@ -1,7 +1,7 @@
 /* Copyright (c) Microsoft Corporation.
    Licensed under the MIT License. */
 
-/** @file */ 
+/** @file */
 
 #ifndef NX_VT_MIDDLEWARE_HELPER_H
 #define NX_VT_MIDDLEWARE_HELPER_H
@@ -31,6 +31,21 @@ UINT nx_azure_iot_pnp_client_reported_component_property_value_next(NX_AZURE_IOT
     const UCHAR** component_pptr,
     UINT* component_len_ptr,
     NX_AZURE_IOT_JSON_READER* name_value_reader_ptr);
+
+/**
+ * @brief Create a telemetry property packet (NX PACKET) to be sent along with telemetry from the properties buffer containing
+ properties (like status of each component) to be sent.
+ *
+ * @param[in] pnp_client_ptr A pointer to a #NX_AZURE_IOT_PNP_CLIENT.
+ * @param[in] component_ptr A pointer to component name for which the property packet needs to be created.
+ * @param[in] component_len_ptr Length of the component name.
+ * @param[out] packet_pptr A pointer telemetry property packet which this function will create.
+ * @param[in] wait_option A NetX library wait option. Defines the number of ThreadX timer ticks to wait for a ping response
+ * @param[in] message_properties_buffer Buffer containing properties to be sent as telemetry property packet.
+ * @param[in] message_properties_buffer_size Size of the buffer containing properties to be sent as telemetry property packet.
+
+ *   @retval #NX_AZURE_IOT_SUCCESS Successful if next desired property is found.
+ */
 
 UINT nx_azure_iot_pnp_client_telemetry_message_create_with_message_property(NX_AZURE_IOT_PNP_CLIENT* pnp_client_ptr,
     const UCHAR* component_name_ptr,
