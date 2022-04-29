44ef7261d8c2e5b1f60e5a2716bb8d672a4c7ccc
Shubhankar Handa <handashubhankar@gmail.com>
2022-04-29 12:49:06 +0530

    test case update

--- tests/core/currentsense/test_vt_cs_object_signature.c
+++ tests/core/currentsense/test_vt_cs_object_signature.c
@@ -215,7 +215,7 @@ static VT_VOID test_vt_currentsense_object_signature_process(VT_VOID** state)
 
     vt_currentsense_object_signature_process(&cs_object);
 
-    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_NOT_MATCHING);
+    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_DB_EMPTY);
 
     cs_object.raw_signatures_reader->repeating_raw_signature_ongoing_collection = false;
     cs_object.mode                                                              = VT_MODE_RUNTIME_EVALUATE;
@@ -256,7 +256,7 @@ static VT_VOID test_vt_currentsense_object_signature_process(VT_VOID** state)
 
     vt_currentsense_object_signature_process(&cs_object);
 
-    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_MATCHING);
+    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_DB_EMPTY);
     // leed to look into cases of mode calib
     cs_object.raw_signatures_reader->repeating_raw_signature_ongoing_collection = false;
     cs_object.mode                                                              = VT_MODE_CALIBRATE;
@@ -380,7 +380,7 @@ static VT_VOID test_vt_currentsense_object_signature_process(VT_VOID** state)
 
     vt_currentsense_object_signature_process(&cs_object);
 
-    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_MATCHING);
+    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_DB_EMPTY);
 
     assert_int_equal(cs_object.mode, VT_MODE_RUNTIME_EVALUATE);
     assert_int_equal(cs_object.db_updated, true);
@@ -424,7 +424,7 @@ static VT_VOID test_vt_currentsense_object_signature_process(VT_VOID** state)
 
     vt_currentsense_object_signature_process(&cs_object);
 
-    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_MATCHING);
+    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_DB_EMPTY);
 
     cs_object.raw_signatures_reader->repeating_raw_signature_ongoing_collection = false;
     cs_object.mode                                                              = VT_MODE_RUNTIME_EVALUATE;
@@ -462,7 +462,7 @@ static VT_VOID test_vt_currentsense_object_signature_process(VT_VOID** state)
 
     vt_currentsense_object_signature_process(&cs_object);
 
-    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_MATCHING);
+    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_DB_EMPTY);
 
     cs_object.raw_signatures_reader->repeating_raw_signature_ongoing_collection = false;
     cs_object.mode                                                              = VT_MODE_CALIBRATE;
@@ -580,7 +580,7 @@ static VT_VOID test_vt_currentsense_object_signature_process(VT_VOID** state)
 
     vt_currentsense_object_signature_process(&cs_object);
 
-    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_MATCHING);
+    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_DB_EMPTY);
 
     cs_object.raw_signatures_reader->repeating_raw_signature_ongoing_collection = false;
     cs_object.mode                                                              = VT_MODE_RUNTIME_EVALUATE;
@@ -619,7 +619,7 @@ static VT_VOID test_vt_currentsense_object_signature_process(VT_VOID** state)
 
     vt_currentsense_object_signature_process(&cs_object);
 
-    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_MATCHING);
+    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_DB_EMPTY);
 
     cs_object.raw_signatures_reader->repeating_raw_signature_ongoing_collection = false;
     cs_object.mode                                                              = VT_MODE_RUNTIME_EVALUATE;
@@ -658,7 +658,7 @@ static VT_VOID test_vt_currentsense_object_signature_process(VT_VOID** state)
 
     vt_currentsense_object_signature_process(&cs_object);
 
-    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_MATCHING);
+    assert_int_equal(cs_object.sensor_status, VT_SIGNATURE_DB_EMPTY);
 
     cs_object.raw_signatures_reader->repeating_raw_signature_ongoing_collection = false;
     cs_object.mode                                                              = VT_MODE_RUNTIME_EVALUATE;
